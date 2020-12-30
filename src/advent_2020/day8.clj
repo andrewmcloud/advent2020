a0b0843b45b3b68b8f7a3c52928c2e6ea4b52020
Andrew McLoud <philip.mcloud@navy.mil>
2020-12-30 16:06:02 -0500

    day8, part2

--- src/advent_2020/day8.clj
+++ src/advent_2020/day8.clj
@@ -16,7 +16,16 @@
   [instructions-map instruction-num]
   (assoc-in instructions-map [instruction-num :visited] true))
 
-(defn- run-program
+(defn- swap-instruction
+  [instructions-map swapped-index]
+  (loop [i swapped-index]
+    (let [instruction (get instructions-map i)]
+      (cond
+        (= "nop" (:instruction instruction)) (assoc-in instructions-map [i :instruction] "jmp")
+        (= "jmp" (:instruction instruction)) (assoc-in instructions-map [i :instruction] "nop")
+        :else (recur (inc i))))))
+
+(defn- run-program-1
   [instructions-map]
   (loop [instructions-map instructions-map
          instruction-num 0
@@ -35,10 +44,45 @@
                                      (+ instruction-num (:jmp instruction-map))
                                      acc)))))
 
+(defn- run-program-2
+  [instructions-map]
+  (loop [instrs-map instructions-map
+         instruction-num 0
+         acc 0
+         swapped-index 0]
+    (let [instruction-map (get instrs-map instruction-num)
+          instruction (:instruction instruction-map)]
+      (cond
+        (nil? instruction-map) acc
+        (true? (:visited instruction-map)) (recur (swap-instruction instructions-map swapped-index)
+                                                  0
+                                                  0
+                                                  (inc swapped-index))
+        (= "nop" instruction) (recur (mark-visited instrs-map instruction-num)
+                                     (inc instruction-num)
+                                     acc
+                                     swapped-index)
+        (= "acc" instruction) (recur (mark-visited instrs-map instruction-num)
+                                     (inc instruction-num)
+                                     (+ acc (:jmp instruction-map))
+                                     swapped-index)
+        (= "jmp" instruction) (recur (mark-visited instrs-map instruction-num)
+                                     (+ instruction-num (:jmp instruction-map))
+                                     acc
+                                     swapped-index)))))
+
 (defn solve-1
   []
   (->> "day8-input.txt"
        read-string-file
        parse-program
        (reduce merge)
-       run-program))
+       run-program-1))
+
+(defn solve-2
+  []
+  (->> "day8-input.txt"
+     read-string-file
+     parse-program
+     (reduce merge)
+     run-program-2))
