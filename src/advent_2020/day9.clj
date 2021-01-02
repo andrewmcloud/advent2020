2fec112096d14ecadba6f84c6249025e305f973d
Andrew McLoud <philip.mcloud@navy.mil>
2021-01-02 09:24:01 -0500

    day9, part2

--- src/advent_2020/day9.clj
+++ src/advent_2020/day9.clj
@@ -19,9 +19,34 @@
         target
         (recur (inc i))))))
 
+(defn- subsequence-sum
+  [target l]
+  (loop [[n & ns] l
+          i 1
+          sum 0
+          subsequence []]
+    (cond
+      (= target sum) subsequence
+      (> target sum) (recur ns i (+ sum n) (conj subsequence n))
+      (< target sum) (recur (drop i l) (inc i) 0 []))))
+
+(defn- encryption-weakness
+  [subsequence]
+  (let [smallest (apply min subsequence)
+        largest (apply max subsequence)]
+    (+ smallest largest)))
+
 (defn solve-1
   []
   (->> "day9-input.txt"
        read-string-file
-       (map #(Integer/parseInt %))
+       (map #(Long/parseLong %))
        (search 25)))
+
+(defn solve-2
+  []
+  (->> "day9-input.txt"
+       read-string-file
+       (map #(Long/parseLong %))
+       (subsequence-sum (solve-1))
+       encryption-weakness))
